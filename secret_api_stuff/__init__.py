b97e4d335e6505d3367991b03e800aa57601265f
Raymond Tana <rjf5371@psu.edu>
2024-09-25 12:44:20 -0400

    removed api

--- secret_api_stuff/__init__.py
+++
@@ -1,2 +0,0 @@
-# here to make sure python treats this like a package
-# "secrets.py" isn't a good name because it conflicts with something else that offers a secrets.choice... avoiding it for now.
--- secret_api_stuff/api_keys.py
+++
@@ -1,3 +0,0 @@
-def get_news_api():
-    return 'e2f29d109cf04ff987e713b82d7539c8'
-
